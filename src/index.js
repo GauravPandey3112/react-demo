ca564c243a8696c3bdab365b8d7515517bacfbc6
GauravPandey3112 <gauravpandey31@yahoo.com>
2021-11-24 22:37:09 +0530

    Change in app

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Context from './Context';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Context>
-      <App />
-    </Context>
+    <App />
 
   </React.StrictMode>,
   document.getElementById('root')
